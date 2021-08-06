b22245f13b24fa3f5f12b8752ae037627f8396ad
TORIBDA <david.moses.toribio@oocl.com>
2021-08-06 14:46:02 +0800

    [ParkingLot][TORIBDA] test: added test case for when should_not_return_any_car_when_fetch_car_given_parking_ticket_is_used_already

--- src/test/java/com/parkinglot/ParkingLot_Test.java
+++ src/test/java/com/parkinglot/ParkingLot_Test.java
@@ -57,4 +57,18 @@ public class ParkingLot_Test {
         //then
         assertNull(actualCar);
     }
+
+    @Test
+    public void should_not_return_any_car_when_fetch_car_given_parking_ticket_is_used_already() throws Exception {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+        Car actualCar = parkingLot.fetchCar(parkingTicket);
+        actualCar = parkingLot.fetchCar(parkingTicket);
+
+        //then
+        assertNull(actualCar);
+    }
 }
