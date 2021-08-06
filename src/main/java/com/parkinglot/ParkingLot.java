7a0b6b2c6364f15b5cb120f6ef4ad14a53a4e231
TORIBDA <david.moses.toribio@oocl.com>
2021-08-06 14:23:59 +0800

    [ParkingLot][TORIBDA] test: added test case for should_return_car_when_fetch_car_given_parking_lot_has_parked_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,4 +4,8 @@ public class ParkingLot {
     public ParkingTicket park(Car car) {
         return new ParkingTicket();
     }
+
+    public Car fetchCar(ParkingTicket parkingTicket) {
+        return null;
+    }
 }
--- src/test/java/com/parkinglot/ParkingLot_Test.java
+++ src/test/java/com/parkinglot/ParkingLot_Test.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLot_Test {
@@ -15,5 +16,18 @@ public class ParkingLot_Test {
 
         //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    public void should_return_car_when_fetch_car_given_parking_lot_has_parked_car() throws Exception {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+        Car bobCar = parkingLot.fetchCar(parkingTicket);
+
+        //then
+        assertEquals(car,bobCar);
     }
 }
