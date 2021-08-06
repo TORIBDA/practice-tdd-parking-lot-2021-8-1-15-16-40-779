c9050334e5f021cdca0f056061b424fd10ccaf14
TORIBDA <david.moses.toribio@oocl.com>
2021-08-06 14:25:24 +0800

    [ParkingLot][TORIBDA] feat: added feature of returning the car when the car is fetched that is aligned with the test case should_return_car_when_fetch_car_given_parking_lot_has_parked_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,11 +1,13 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    private Car car;
     public ParkingTicket park(Car car) {
+        this.car = car;
         return new ParkingTicket();
     }
 
     public Car fetchCar(ParkingTicket parkingTicket) {
-        return null;
+        return car;
     }
 }
