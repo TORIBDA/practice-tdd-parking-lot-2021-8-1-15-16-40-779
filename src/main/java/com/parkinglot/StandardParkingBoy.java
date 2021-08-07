33ec3eb9e043f724b8a7f621d23c495caa952cf0
TORIBDA <david.moses.toribio@oocl.com>
2021-08-07 18:26:34 +0800

    [ParkingLot][TORIBDA] chore: removed unnecessary imports and format line indention.

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -1,6 +1,5 @@
 package com.parkinglot;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class StandardParkingBoy extends ParkingBoy {
@@ -13,7 +12,8 @@ public class StandardParkingBoy extends ParkingBoy {
         super(parkingLots);
     }
 
-    public ParkingTicket park(Car car){
+    @Override
+    public ParkingTicket park(Car car) {
         return getParkingLots().stream().filter(parkingLot -> !parkingLot.isParkingLotFull())
                 .findFirst()
                 .orElseThrow(NoAvailablePositionException::new).park(car);
